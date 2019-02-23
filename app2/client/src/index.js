976cfde26bc8589047bce1b5eb1fbc80ae49de25
Steven Hankin <steven.hankin@gmail.com>
2019-02-23 10:00:54 +0000

    feat: Added free Cerulean theme
    Bootstrap theme

--- app2/client/src/index.js
+++ app2/client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './cerulean.min.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
